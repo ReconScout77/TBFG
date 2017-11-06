a808fc4fb9d963bdc1358df96bb00b14c1d119a2
Robert Murray <alexmurray93@hotmail.com>
2017-11-06 11:27:47 -0800

    Initial commit.

---
+++ TBFG/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TBFG.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
---
+++ TBFG/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TBFG.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Username")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password does not match the confirmation password. Try Again.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
